ac5ab47a3feba2c2f76656d55bd008372383aa56
Deepak Kumar <kd134994@gmail.com>
2026-09-20 07:00:34 +0530

    Add files via upload

---
+++ Multiplay.java
@@ -0,0 +1,15 @@
+public class Multiplay {
+
+    public static int multiplay(int a, int b){
+        int product= a*b;
+        return product;
+    }
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        int prod= multiplay(a, b);
+        System.out.println("the multiplay of a*b is: "+prod);
+        
+
+    }
+}
